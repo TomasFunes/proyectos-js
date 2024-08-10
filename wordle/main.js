9dd27250376bd8401d8bdea2472dd85ec89137a4
Tomas <funes.tomas99@gmail.com>
2024-08-10 20:41:27 -0300

    Nuevas funciones y correccion de errores menores

--- wordle/main.js
+++ wordle/main.js
@@ -5,34 +5,52 @@ const gameState = {
     word: getWord(),
 }
 
+function addLetterEvents(guess) {
+    for(const letter of guess.children) {
+        letter.addEventListener("click", handleLetterClick);
+    }
+}
+
 
 function start() {
     const keyboard = document.querySelectorAll('.key');
 
-    for(const letter of gameState.activeGuess.children) {
-        letter.addEventListener("click", handleLetterClick);
-    }
+    addLetterEvents(gameState.activeGuess);
 
     for (const key of keyboard) {
-        key.addEventListener("click", event => {
-            if (event.target.textContent == "Enviar") {
-                checkGuess();
-            } else if (event.target.textContent == "<=") {
-                if(gameState.activeLetter.textContent == "") {
-                    const prevLetter = gameState.activeLetter.previousElementSibling;
-                    setActiveLetter(prevLetter);
-                }
-                gameState.activeLetter.textContent = "";
-
-            } else {
-                const nextLetter = gameState.activeLetter.nextElementSibling;
-                gameState.activeLetter.textContent = key.textContent;
-                setActiveLetter(nextLetter);
-            }
-        })
+        key.addEventListener("click", handleKeyClick);
     }
 }
 
+function end() {
+    const keyboard = document.querySelectorAll('.key');
+
+    for (const key of keyboard) {
+        key.removeEventListener("click", handleKeyClick);
+    }
+}
+
+
+function mapKeyboard (key) {
+    if (key.id == "send") {
+        checkGuess();
+    } else if (key.id == "backspace") {
+        if(gameState.activeLetter.textContent == "") {
+            const prevLetter = gameState.activeLetter.previousElementSibling;
+            setActiveLetter(prevLetter);
+        }
+        gameState.activeLetter.textContent = "";
+
+    } else {
+        gameState.activeLetter.textContent = key.textContent;
+        setActiveLetter(gameState.activeLetter.nextElementSibling);
+    }
+}
+
+
+function handleKeyClick(event) {
+    mapKeyboard(event.currentTarget);
+}
 
 
 function handleLetterClick(event) {
@@ -40,15 +58,21 @@ function handleLetterClick(event) {
 }
 
 function setActiveLetter(letter) {
-    gameState.activeLetter.classList.remove('active-letter');
     if(letter) {
+        gameState.activeLetter.classList.remove('active-letter');
         letter.classList.add('active-letter');
         gameState.activeLetter = letter;
     }
 }
 
+async function wordExists(word) {
+    const response = await fetch('word.json');
+    const dictionary = await response.json();
+
+    return dictionary.includes(word.toLowerCase());
+}
+
 async function getWord() {
-    const msg = document.createElement('p');
     const response = await fetch('word.json');
     const words = await response.json();
     
@@ -58,17 +82,26 @@ async function getWord() {
 
 async function checkGuess() {
     const letters = gameState.activeGuess.children;
+    let stringWord = "";
+
+    for (const letter of letters) {
+        stringWord += letter.textContent;
+    }
+
+    const exists = await wordExists(stringWord);
+    if (!exists) {
+        alert("La palabra no existe (O por lo menos no en el contexto de este juego)");
+        return;
+    }
+
     let word = await gameState.word;
     word = word.toUpperCase();
   
     for (let n = 0; n < letters.length; n++) {
-        if (letters[n].textContent == word[n]){
-            letters[n].style.backgroundColor = "green";
-        } else if (word.includes(letters[n].textContent)) {
-            letters[n].style.backgroundColor = "#bfb41b";
-        } else {
-            letters[n].style.backgroundColor = "grey";
-        }
+        if (letters[n].textContent == word[n]) letters[n].style.backgroundColor = "green";
+        else if (word.includes(letters[n].textContent)) letters[n].style.backgroundColor = "#bfb41b";
+        else letters[n].style.backgroundColor = "grey";
+        
     }
     checkWinCondition(letters);
 }
@@ -78,17 +111,15 @@ async function checkWinCondition(letters) {
     for (const letter of letters) {
         if (letter.style.backgroundColor !== "green") {
             gameState.guesses++;
-            if(gameState.guesses == 6) {
-                alert(`Perdiste. La palabra es: ${await gameState.word}`);
-            } else {
-                setNextGuess();
-                setActiveLetter(gameState.activeGuess.firstElementChild);
-
-            }
+            setNextGuess();
+            
+            if(gameState.guesses == 6) alert(`Perdiste. La palabra es: ${await gameState.word}`);
             return;
         }
     }
     alert(`Correcto! La palabra es: ${await gameState.word}`);
+    setActiveLetter(null);
+
 }
     
 
@@ -101,11 +132,17 @@ function setNextGuess() {
 
     currentGuess = currentGuess.nextElementSibling;
 
-    for(const letter of currentGuess.children) {
-        letter.addEventListener("click", handleLetterClick);
+    if (currentGuess) {
+        for(const letter of currentGuess.children) {
+            letter.addEventListener("click", handleLetterClick);
+        }
+        
+        gameState.activeGuess = currentGuess;
+        setActiveLetter(currentGuess.firstElementChild);
+    } else {
+        end();
     }
 
-    gameState.activeGuess = currentGuess;
 }
 
 start();
